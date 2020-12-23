874d5c0805a567117f4e9ca6ec228bc71317c277
PJ Evans <pj@mrpjevans.com>
2020-12-23 16:59:20 +0000

    Correct LED pin for green

--- game.py
+++ game.py
@@ -5,7 +5,7 @@ from gpiozero import LED, Buzzer, Button
 leds = {
     "red": LED(18),
     "yellow": LED(23),
-    "green": LED(34)
+    "green": LED(24)
 }
 bz = Buzzer(21)
 button = Button(25)
@@ -13,18 +13,18 @@ button = Button(25)
 choice = "red"
 
 
-def check_colour:
-    if choice is not "green":
-        bz.on
+def check_colour():
+    if choice != "green":
+        bz.on()
         sleep(0.5)
-        bz.off
+        bz.off()
 
 
 button.when_pressed = check_colour
 
 while True:
     for led in leds:
-        led.off()
+        leds[led].off()
 
     choice = random.choice(["red", "yellow", "green"])
     leds[choice].on()
